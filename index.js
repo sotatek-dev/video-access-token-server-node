2116d4891540a3cb588a5bae900cc9b5a50222c8
An Nguyen <nguyenhuuan@gmail.com>
2017-06-29 14:10:16 +0700

    Some changes of TuyenLC.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 const tokenGenerator = require('./src/token_generator');
+var randomstring = require("randomstring");
 
 // Create Express webapp
 const app = express();
@@ -14,6 +15,18 @@ app.get('/', function(request, response) {
   const identity = request.query.identity || 'identity';
   const room = request.query.room;
   response.send(tokenGenerator(identity, room));
+});
+
+app.get('/token', function(request, response) {
+    var identity = randomstring.generate();
+
+    const room = request.query.room;
+
+    // Serialize the token to a JWT string and include it in a JSON response
+    response.send({
+        identity: identity,
+        token: tokenGenerator(identity, room)
+    });
 });
 
 // Create an http server and run it
