f9420fcf75cf0253fe3d49c9b1b3e2d20ee847ad
An Nguyen <nguyenhuuan@gmail.com>
2017-06-29 14:13:40 +0700

    Run on a different port.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/token', function(request, response) {
 
 // Create an http server and run it
 const server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 server.listen(port, function() {
   console.log('Express server running on *:' + port);
 });
